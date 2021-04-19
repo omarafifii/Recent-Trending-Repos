4dff9a78ce9dbe77b77649e570cdf711299efa12
omarafifii <omarafifi@windowslive.com>
2021-04-20 01:27:43 +0200

    created function to process the data

--- data_wrangling.py
+++ data_wrangling.py
@@ -9,3 +9,10 @@ def get_data():
     start_date = start_date.strftime('%Y-%m-%d')
     r1 = requests.get('https://api.github.com/search/repositories?q=created:>{}&sort=stars&order=desc'.format(start_date))
     return r1
+
+def process_data(r1):
+    r1_json = json.loads(r1.text)
+    r1_df = pd.json_normalize(r1_json, record_path=['items'])
+    r2_df = r1_df[['name','language']]
+    language_list_df = r2_df.groupby('language').count()
+    return language_list_df, r2_df
