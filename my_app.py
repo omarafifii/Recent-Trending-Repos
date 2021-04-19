68cbe7855660bb7ed7b4d90ac8300af2c2a7b205
omarafifii <omarafifi@windowslive.com>
2021-04-20 01:38:23 +0200

    add functions from data_wrangling.py to flask app

---
+++ my_app.py
@@ -0,0 +1,13 @@
+from flask import Flask,jsonify
+import data_wrangling as dw
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    data = dw.get_data()
+    language_list_df, data_df = dw.process_data(data)
+    output = dw.data_to_json(language_list_df, data_df)
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
