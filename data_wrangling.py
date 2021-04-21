bcf86f79cc6a4e50605ae6eee7b605af7e88138e
omarafifii <omarafifi@windowslive.com>
2021-04-22 00:00:05 +0200

    bug fix + added code comments for documentation

--- data_wrangling.py
+++ data_wrangling.py
@@ -5,12 +5,24 @@ import datetime
 import json
 
 def get_data():
+    """
+    This funtion is used to get data from the github API.
+    --------------------------------
+    returns the response for the request
+    """
     start_date = datetime.datetime.now() - datetime.timedelta(days=30)
     start_date = start_date.strftime('%Y-%m-%d')
     r1 = requests.get('https://api.github.com/search/repositories?q=created:>{}&sort=stars&order=desc'.format(start_date))
     return r1
 
 def process_data(r1):
+    """
+    This funtion is used to process the data to achieve the desired results.
+    --------------------------------
+    params: The response from the request sent to github API.
+    --------------------------------
+    returns two dataframes containing the desired results.
+    """
     r1_json = json.loads(r1.text)
     r1_df = pd.json_normalize(r1_json, record_path=['items'])
     r2_df = r1_df[['name','language']]
@@ -18,11 +30,18 @@ def process_data(r1):
     return language_list_df, r2_df
 
 def data_to_json(language_list_df, r2_df):
+    """
+    This funtion is used to change the data from the dataframes to a json format
+    --------------------------------
+    params: The two dataframes containing the data.
+    --------------------------------
+    returns json object containing the data.
+    """
     output = {}
     language_list = language_list_df.index.tolist()
     for language in language_list:
         single_output = {}
-        single_output['number'] = language_list_df.loc[language,'name']
-        single_output['list'] = r2_df[r2_df.language == language]['name'].tolist()
+        single_output['number_of_repos'] = str(language_list_df.loc[language,'name'])
+        single_output['list_of_repos'] = r2_df[r2_df.language == language]['name'].tolist()
         output[language] = single_output
     return output
--- my_app.py
+++ my_app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """
+    This funtion is called when the root of the application url is requested
+    """
     data = dw.get_data()
     language_list_df, data_df = dw.process_data(data)
     output = dw.data_to_json(language_list_df, data_df)
